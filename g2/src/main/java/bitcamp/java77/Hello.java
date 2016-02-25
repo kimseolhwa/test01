da6151402122b84331988817fe52454734d6c298
cheonok <cheon_ok@naver.com>
2016-02-25 20:47:56 +0900

    .

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -1,10 +1,9 @@
 package bitcamp.java77;
 
-
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("홍길동");
+		System.out.println("천옥");
 		System.out.println("kimyk");
-
 	}
 }
