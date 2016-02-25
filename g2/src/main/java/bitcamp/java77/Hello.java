dd9d98e295bac636ff2d73a85ebdb40e9a09b66a
kimseolhwa <ratfg31@naver.com>
2016-02-25 20:50:53 +0900

    .

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -3,7 +3,11 @@ package bitcamp.java77;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("홍길동");
+
+		System.out.println("안녕");
+
 		System.out.println("천옥");
+
 		System.out.println("kimyk");
 	}
 }
